d46278b5051968d4d8f2f8ec2446611f632bf0ad
Bruno Rodrigues <brunohrsilva1@gmail.com>
2022-08-27 01:49:36 -0300

    Add shared config

---
+++ src/shared/constants.ts
@@ -0,0 +1,9 @@
+export const FONT_WEIGHTS = {
+	inherit: '',
+	light: 'font-light',
+	normal: 'font-normal',
+	medium: 'font-medium',
+	semibold: 'font-semibold',
+	bold: 'font-bold',
+	extrabold: 'font-extrabold',
+};
