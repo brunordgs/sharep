451b93b6786425db10bcfdc3061bf398ebbffdb8
Bruno Rodrigues <brunohrsilva1@gmail.com>
2022-09-08 13:02:43 -0300

    Add session into navbars

--- src/components/Header/MobileNavbar.tsx
+++ src/components/Header/MobileNavbar.tsx
@@ -1,9 +1,13 @@
+import { signIn, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { List } from 'phosphor-react';
-import { Container } from '../Container';
+import { Container } from '../ui/Container';
 import { Button } from '../ui/Buttons/Button';
+import { UserDropdown } from '../User/UserDropdown';
 
 export function MobileNavbar() {
+	const { data: session } = useSession();
+
 	return (
 		<Container className="flex items-center justify-between lg:hidden">
 			<Link href="/">
@@ -13,9 +17,14 @@ export function MobileNavbar() {
 			</Link>
 
 			<div className="flex items-center gap-4">
-				<Button type="button" variant="outlined">
-					Sign in
-				</Button>
+				{!session ? (
+					<Button variant="outlined" onClick={() => signIn('github')}>
+						Sign in
+					</Button>
+				) : (
+					<UserDropdown avatar={session?.user?.image!} />
+				)}
+
 				<Button type="button" color="unstyled">
 					<List size={24} />
 				</Button>
--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,58 +1,88 @@
+import clsx from 'clsx';
+import { signIn, useSession } from 'next-auth/react';
 import Link from 'next/link';
-import { Article, MagnifyingGlass, Question } from 'phosphor-react';
-import { Container } from '../Container';
+import { useRouter } from 'next/router';
+import { Article, MagnifyingGlass, PaintBrush, Question } from 'phosphor-react';
 import { Button } from '../ui/Buttons/Button';
+import { Container } from '../ui/Container';
+import { UserDropdown } from '../User/UserDropdown';
 import { MobileNavbar } from './MobileNavbar';
 
 export function Navbar() {
+	const router = useRouter();
+	const { data: session } = useSession();
+
+	const menuItems = [
+		{
+			link: '/',
+			text: 'Projects',
+			icon: Article,
+			isActive: router.pathname === '/',
+		},
+		{
+			link: '/creators',
+			text: 'Creators',
+			icon: PaintBrush,
+			isActive: router.pathname === '/creators',
+		},
+		{
+			link: '/about',
+			text: 'About',
+			icon: Question,
+			isActive: router.pathname === '/about',
+		},
+	];
+
 	return (
-		<header className="border-b border-zinc-200 shadow-sm py-5">
+		<header className="border-b border-zinc-200 dark:border-zinc-800 shadow-sm py-5">
 			{/* Desktop navbar */}
 			<Container className="hidden lg:flex items-center">
 				<div className="flex-1">
 					<nav className="flex">
 						<Link href="/">
-							<a className="text-2xl italic font-bold text-rose-600 hover:text-rose-500 transition-colors duration-300">
+							<a className="text-2xl italic font-bold text-rose-600 dark:text-rose-500 hover:text-rose-500 dark:hover:text-rose-600 transition-colors duration-300">
 								sharep
 							</a>
 						</Link>
 
-						<ul className="flex items-center gap-6 border-l-4 border-rose-600 px-8 ml-8 font-bold text-zinc-800">
-							<li>
-								<Link href="/">
-									<a className="flex items-center gap-2">
-										<Article size={20} weight="bold" />
-										Projects
-									</a>
-								</Link>
-							</li>
-
-							<li>
-								<Link href="/about">
-									<a className="flex items-center gap-2">
-										<Question size={20} weight="bold" />
-										About
-									</a>
-								</Link>
-							</li>
+						<ul className="flex items-center gap-6 border-l-4 border-rose-600 px-8 ml-8 font-bold text-zinc-700 dark:text-zinc-300">
+							{menuItems.map(({ link, text, icon: Icon, isActive }) => (
+								<li key={link}>
+									<Link href={link}>
+										<a
+											className={clsx(
+												isActive && 'text-black dark:text-white',
+												'flex items-center gap-2 hover:text-black dark:hover:text-white transition-colors duration-300',
+											)}
+										>
+											<Icon size={20} weight="bold" />
+											{text}
+										</a>
+									</Link>
+								</li>
+							))}
 						</ul>
 					</nav>
 				</div>
 
 				<div className="flex items-center gap-4">
-					<div className="flex items-center h-10 w-56 bg-zinc-200 gap-2 px-4 rounded-md">
-						<MagnifyingGlass weight="bold" size={24} className="text-zinc-600" />
+					<div className="flex items-center h-10 w-56 bg-zinc-200 dark:bg-zinc-700 gap-2 px-4 rounded-md">
+						<MagnifyingGlass weight="bold" size={24} className="text-zinc-600 dark:text-zinc-500" />
 
 						<input
 							type="text"
-							className="bg-zinc-200 px-2 w-full h-full focus:outline-none placeholder:text-zinc-400"
+							className="bg-zinc-200 dark:bg-zinc-700 px-2 w-full h-full focus:outline-none placeholder:text-zinc-400 dark:placeholder:text-zinc-500"
 							placeholder="Search..."
 						/>
 					</div>
 
-					<Button type="button" variant="outlined">
-						Sign in
-					</Button>
+					{!session ? (
+						<Button variant="outlined" onClick={() => signIn('github')}>
+							Sign in
+						</Button>
+					) : (
+						<UserDropdown avatar={session?.user?.image!} />
+					)}
 				</div>
 			</Container>
 
