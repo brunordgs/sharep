7561628bced5404ab0b2c9dd9b4f1e335c8ea508
Bruno Rodrigues <brunohrsilva1@gmail.com>
2022-09-08 13:00:57 -0300

    Move components to ui folder

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -3,7 +3,7 @@ import { HtmlHTMLAttributes } from 'react';
 
 export function Card({ className, children }: HtmlHTMLAttributes<HTMLDivElement>) {
 	return (
-		<div className={clsx('bg-white rounded-md p-6 border border-zinc-200 shadow-sm', className)}>
+		<div className={clsx('bg-white dark:bg-zinc-800 rounded-md p-6 border border-zinc-200 dark:border-zinc-700 shadow-sm', className)}>
 			{children}
 		</div>
 	);
