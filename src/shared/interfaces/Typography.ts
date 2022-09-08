0676f87803c4e9b6b9335524e86a49799e79d646
Bruno Rodrigues <brunohrsilva1@gmail.com>
2022-09-08 12:54:54 -0300

    Add italic text style

--- src/shared/interfaces/Typography.ts
+++ src/shared/interfaces/Typography.ts
@@ -2,5 +2,5 @@ import { FONT_WEIGHTS } from "../constants";
 
 export interface Typography {
 	weight?: keyof typeof FONT_WEIGHTS;
-	transform?: 'normal-case' | 'uppercase' | 'lowercase' | 'capitalize';
+	transform?: 'normal-case' | 'uppercase' | 'lowercase' | 'capitalize' | 'italic';
 }
