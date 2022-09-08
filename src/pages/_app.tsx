2d78eba3dbc92826fc0e35a1c6748d73047707b2
Bruno Rodrigues <brunohrsilva1@gmail.com>
2022-09-08 13:00:11 -0300

    Add next auth session provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,18 @@
-import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
 import { Footer } from '@/components/Footer';
 import { Navbar } from '@/components/Header/Navbar';
 import '@/styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 	const router = useRouter();
 
 	return (
-		<>
+		<SessionProvider session={session}>
 			<Navbar />
 			<Component {...pageProps} />
 			{router.pathname !== '/' && <Footer />}
-		</>
+		</SessionProvider>
 	);
 }
