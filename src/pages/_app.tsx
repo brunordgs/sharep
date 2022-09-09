2a67ed01e8265b6d77082d068a362dc2ddd90da0
Bruno Rodrigues <brunohrsilva1@gmail.com>
2022-09-08 22:10:41 -0300

    Exclude routes to prevent additional footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,23 @@
 import { Footer } from '@/components/Footer';
 import { Navbar } from '@/components/Header/Navbar';
 import '@/styles/globals.css';
+import clsx from 'clsx';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
 	const router = useRouter();
+	const excludeRoutes = ['/', '/creators'];
 
 	return (
 		<SessionProvider session={session}>
 			<Navbar />
 			<Component {...pageProps} />
-			{router.pathname !== '/' && <Footer />}
+
+			<div className={clsx({ 'lg:hidden': excludeRoutes.includes(router.pathname) })}>
+				<Footer />
+			</div>
 		</SessionProvider>
 	);
 }
