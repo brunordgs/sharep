0f2e461042a3830968bc73d1d6d4a68f41e646a2
Bruno Rodrigues <brunohrsilva1@gmail.com>
2022-09-09 01:50:55 -0300

    Update custom 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,21 @@
+import { Heading } from '@/components/ui/Typography/Heading';
+import { Text } from '@/components/ui/Typography/Text';
+
 export default function Custom404() {
-	return <div>404</div>;
+	return (
+		<main className="flex flex-col justify-center text-center my-10">
+			<div className="mb-8">
+				<Heading size="xl" transform="italic" className="md:text-5xl lg:text-7xl">
+					404.
+				</Heading>
+				<Heading as="h2" size="sm" transform="italic" className="md:text-xl lg:text-3xl">
+					Oops! Page not found
+				</Heading>
+			</div>
+
+			<Text size="sm" className="md:text-base">
+				We can't seem to find the page you are looking for
+			</Text>
+		</main>
+	);
 }
