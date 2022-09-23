cc57ead2999699f1fe122b88e0c871013079530e
Bruno Rodrigues <brunohrsilva1@gmail.com>
2022-09-23 00:39:03 -0300

    Update revalidate time

--- src/pages/[profile].tsx
+++ src/pages/[profile].tsx
@@ -97,6 +97,6 @@ export async function getStaticProps({ params }: GetStaticPropsContext) {
 			user: userData[0],
 			creator: creatorData?.[0] ?? null,
 		},
-		revalidate: 60 * 60, // 1 hour
+		revalidate: 60 * 5, // 5 min
 	};
 }
