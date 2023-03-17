6baaf7dfaee0b99ca775f287abe03a94bed785bc
Bruno Rodrigues <brunohrsilva1@gmail.com>
2023-03-17 01:04:03 -0300

    Add custom 500 error page

---
+++ src/pages/500.tsx
@@ -0,0 +1,28 @@
+import { Heading } from '@/components/ui/Typography/Heading';
+import { Text } from '@/components/ui/Typography/Text';
+import Head from 'next/head';
+
+export default function Custom500() {
+	return (
+		<>
+			<Head>
+				<title>Something is wrong | sharep</title>
+			</Head>
+
+			<main className="flex flex-col justify-center text-center my-10">
+				<div className="mb-8">
+					<Heading size="xl" transform="italic" className="md:text-5xl lg:text-7xl">
+						500.
+					</Heading>
+					<Heading as="h2" size="sm" transform="italic" className="md:text-xl lg:text-3xl">
+						Oops! Something is wrong
+					</Heading>
+				</div>
+
+				<Text size="sm" className="md:text-base">
+					We're having some issues at the moment. Please try again.
+				</Text>
+			</main>
+		</>
+	);
+}
