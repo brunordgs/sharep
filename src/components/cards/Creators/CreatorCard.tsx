ef8c7b49a914e10f444f8272b585a89c9e171a8f
Bruno Rodrigues <brunohrsilva1@gmail.com>
2024-02-25 23:22:01 -0300

    Ignore folder sensitive case

---
+++ src/components/cards/Creators/CreatorCard.tsx
@@ -0,0 +1,44 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { getFallbackInitials } from '@/utils/helpers/format';
+import { CircleWavyCheck } from '@phosphor-icons/react/dist/ssr';
+import { Text } from '@ui/Typography/Text';
+import Link from 'next/link';
+
+interface Props {
+	name: string;
+	username: string;
+	isVerified: boolean;
+	avatar: string;
+}
+
+export function CreatorCard({ name, username, isVerified, avatar }: Props) {
+	return (
+		<Link
+			key={username}
+			href={`/@${username}`}
+			className="border-t border-zinc-200 first:border-0 dark:border-zinc-700 p-6 flex justify-between hover:bg-zinc-50 dark:hover:bg-zinc-700 w-full h-28 transition-colors ease-out"
+		>
+			<div className="flex items-center gap-4">
+				<Avatar>
+					<AvatarImage src={avatar} alt={name} />
+					<AvatarFallback>{getFallbackInitials(name)}</AvatarFallback>
+				</Avatar>
+
+				<div>
+					<div className="flex items-center gap-1">
+						<Text weight="bold">{name}</Text>
+						{isVerified && (
+							<CircleWavyCheck
+								weight="fill"
+								className="text-indigo-500"
+								aria-label="Verified account"
+							/>
+						)}
+					</div>
+
+					<Text size="sm">@{username}</Text>
+				</div>
+			</div>
+		</Link>
+	);
+}
